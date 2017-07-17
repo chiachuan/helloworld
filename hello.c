6d198c2fce3b7bf860720dee12ce981bf2594b21
Wu Chia Chuan <chiachuan.wu@luxoft.com>
2017-07-17 11:23:10 +0800

    this is the new changes to git pull testing

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ int main(void)
 	sleep(100000000);
 	printf("Hello World!!\n");
         print("hello world!!\n");        
-	#currently doing git pull testing#	
+	#currently doing git pull testing##	
 	__mwait(0,0);
 
 	sleep(10);
